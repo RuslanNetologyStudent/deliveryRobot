aff59a133d83048ba56f572f8d94d760a806dd15
Training JAVA_programming <graf1324@mail.ru>
2023-06-26 21:35:52 +0700

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> {
+                int score = 0;
+                String route = generateRoute("RLRFR", 100);
+                char[] chars = route.toCharArray();
+                for (char symbol : chars) {
+                    if (symbol == 'R') {
+                        score++;
+                    }
+                }
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.containsKey(score)) {
+                        int value = sizeToFreq.get(score);
+                        value++;
+                        sizeToFreq.put(score, value);
+                    } else {
+                        sizeToFreq.put(score, 1);
+                    }
+                }
+            }).start();
+        }
+
+        int maxValue = Collections.max(sizeToFreq.values());
+
+        List<Integer> maxValueKeys = sizeToFreq.entrySet()
+                .stream()
+                .filter(x -> x.getValue() == maxValue)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        System.out.println("Самое частое количество повторений " + maxValueKeys + " (встретилось " +
+                sizeToFreq.get(maxValueKeys.get(0)) + " раз)");
+        System.out.println("Другие размеры:");
+        sizeToFreq.entrySet()
+                .stream()
+                .filter(entry -> !maxValueKeys.contains(entry.getKey()))
+                .forEach(entry -> System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)"));
+    }
+
+    private static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+}
